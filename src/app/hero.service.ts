2385cbe694bce7c751270cdb7a5ddbf23ff79419
Gg <58531623+djghis@users.noreply.github.com>
2020-12-27 17:07:55 +0000

    app is nearly done but moving from database on computer to http (using in memory api)

---
+++ src/app/hero.service.ts
@@ -0,0 +1,38 @@
+import { MessageService } from './message.service';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+constructor(
+  private http: HttpClient,
+  private messageService: MessageService) { }
+
+getHeroes(): Observable<Hero[]> {
+  // TODO: send the message _after_ fetching the heroes
+  this.messageService.add('HeroService: fetched heroes');
+  return of(HEROES);
+}
+
+getHero(id: number): Observable<Hero> {
+  // TODO: send the message _after_ fetching the hero
+  this.messageService.add(`HeroService: fetched hero id=${id}`);
+  return of(HEROES.find(hero => hero.id === id));
+}
+
+/** Log a HeroService message with the MessageService */
+private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+}
+
+}
